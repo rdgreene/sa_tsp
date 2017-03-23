7e74737e79b584dc736d9869fa54fbc91c46be68
Ronan Greene <Ronan Greene>
2017-03-23 19:57:04 +0000

    fixed bug in loadTSPmatrix.py

--- loadTSPmatrix.py
+++ loadTSPmatrix.py
@@ -3,6 +3,14 @@
 Created on Thu Mar 23 13:40:00 2017
 
 @author: ronan
+
+
+Use following to implement in main script:
+
+from loadTSPmatrix import loadTSPmatrix
+file_name = 'filename and directory here'
+TPSmatrix = loadTSPmatrix(file_name)
+
 """
 
 def loadTSPmatrix(file_name):
@@ -19,6 +27,12 @@ def loadTSPmatrix(file_name):
     for i in range(0, dimensions):
         for j in range(0, dimensions):
             matrix[i, j] = 0 - matrix[i, j] 
+            
+    # add additional colum and row to array for returning to start state
+    row = np.array([0]*dimensions)
+    col = np.array([0]*(dimensions+1))
+    matrix = np.row_stack((matrix,row)) 
+    matrix = np.column_stack((matrix,col)) 
     
     # set 'diagonol' to nan
     for i in range(0, dimensions):
@@ -26,5 +40,11 @@ def loadTSPmatrix(file_name):
     
     # add end row of nan values    
     matrix[:,-1] = np.nan
+    
+    # set end column to nan
+    matrix[:,-1] = np.nan
+
+    # set end row to nan (with exception of first element)
+    matrix[-1, 1:-1] = np.nan
     
     return matrix
